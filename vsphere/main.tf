f5f3d91d76779a3c1c615038f4d36c6fa9d98892
Rudi MK <rudimk.cloud@gmail.com>
2021-07-07 18:04:55 +0530

    Modified the Terraform plan to pick up vCPU, RAM and disk values as TF_VARS. Also added a provisioner that injects a user-provided SSH key, and resizes the root disk.

--- vsphere/main.tf
+++ vsphere/main.tf
@@ -50,6 +50,30 @@ variable "vm_ip" {
   type = string
 }
 
+variable "vm_vcpu" {
+  type = number
+}
+
+variable "vm_ram" {
+  type = number
+}
+
+variable "vm_disk" {
+  type = number
+}
+
+variable "ssh_user" {
+  type = string
+}
+
+variable "ssh_passwd" {
+  type = string
+}
+
+variable "ssh_pub_key" {
+  type = string
+}
+
 
 provider "vsphere" {
   user           = var.vsphere_user
@@ -89,8 +113,8 @@ resource "vsphere_virtual_machine" "vm" {
   resource_pool_id = "${data.vsphere_compute_cluster.cluster.resource_pool_id}"
   datastore_id     = "${data.vsphere_datastore.datastore.id}"
 
-  num_cpus = 1
-  memory   = 2048
+  num_cpus = var.vm_vcpu
+  memory   = var.vm_ram
   guest_id = var.vm_guest
   host_system_id = var.vsphere_host
   #vvtd_enabled = false
@@ -104,10 +128,11 @@ resource "vsphere_virtual_machine" "vm" {
 
   disk {
     label            = "disk0"
-    size             = "${data.vsphere_virtual_machine.template.disks.0.size}"
+    size             = var.vm_disk
     eagerly_scrub    = "${data.vsphere_virtual_machine.template.disks.0.eagerly_scrub}"
     thin_provisioned = "${data.vsphere_virtual_machine.template.disks.0.thin_provisioned}"
   }
+
 
   clone {
     template_uuid = "${data.vsphere_virtual_machine.template.id}"
@@ -126,5 +151,21 @@ resource "vsphere_virtual_machine" "vm" {
 
       ipv4_gateway = "172.31.31.254"
     }
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "mkdir -p /home/${var.ssh_user}/.ssh",
+      "touch /home/${var.ssh_user}/.ssh/authorized_keys",
+      "echo ${var.ssh_pub_key} >> /home/${var.ssh_user}/.ssh/authorized_keys",
+      "sudo growpart /dev/sda 2",
+      "sudo resize2fs /dev/sda2"
+    ]
+  }
+  connection {
+    host = var.vm_ip
+    type = "ssh"
+    user = var.ssh_user
+    password = var.ssh_passwd
   }
 }
